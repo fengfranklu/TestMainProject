057dd9e8e356de6f046cc46fc5043f65512ad102
Feng Lu <fengfranklu@gmail.com>
2020-12-04 22:35:15 -0500

    change fonts

--- TestMainProject/MainWindow.xaml.cs
+++ TestMainProject/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-using TestSubProject;
+//using TestSubProject;
 
 namespace TestMainProject
 {
